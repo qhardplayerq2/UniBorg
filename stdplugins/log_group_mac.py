6c276724d498dbdf2cb66e545e6857c2c4d86ef3
hamitdurmus <55110331+hamitdurmus@users.noreply.github.com>
2020-02-05 20:13:47 +0300

    Update log_group_mac.py

--- stdplugins/log_group_mac.py
+++ stdplugins/log_group_mac.py
@@ -25,7 +25,7 @@ async def monito_p_m_s(event):
     # print(me.id)
     # if  event.text == ".loggroups":
     
-    link_detect = re.findall(r'([0-9a-f]{2}(?::[0-9a-f]{2}){5})',event.message.message)
+    link_detect = re.findall(r'([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])',event.message.message)
     # print(link_detect)
     # print("logging success")
     # await event.edit("loggin success")
