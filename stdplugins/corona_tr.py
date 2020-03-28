6adac192664b0da7a1b3dc7754df683b98dbb7c3
M.Furkan <byazade@yandex.com>
2020-03-28 20:15:42 +0300

    edit

--- stdplugins/corona_tr.py
+++ stdplugins/corona_tr.py
@@ -17,11 +17,11 @@ async def cor_tr(event):
     r = requests.get(
     'https://covid19.saglik.gov.tr/')
     if r.status_code == 200:
-        resim1 = "https://covid19.saglik.gov.tr/1.jpg"
+        resim1 = "https://covid19.saglik.gov.tr/img/1.jpg"
         res1 = requests.get(resim1)
         if res1.status_code == 200:
             wget.download(resim1, out='./DOWNLOADS/1.jpg')
-        resim2 = "https://covid19.saglik.gov.tr/2.jpg"
+        resim2 = "https://covid19.saglik.gov.tr/img/2.jpg"
         res2 = requests.get(resim2)
         if res2.status_code == 200:
             wget.download(resim2, out='./DOWNLOADS/2.jpg')
