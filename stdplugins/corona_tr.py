3b9af53bb343f8d39fc09883840d4634b24bef6d
M.Furkan <byazade@yandex.com>
2020-04-06 20:45:20 +0300

    new corona

--- stdplugins/corona_tr.py
+++ stdplugins/corona_tr.py
@@ -1,55 +1,68 @@
 import asyncio
+import os
+from HTMLParser import HTMLParseError
 
 import requests
 
 import wget
+from bs4 import BeautifulSoup
 from sample_config import Config
 from uniborg.util import admin_cmd
-import os
-from bs4 import BeautifulSoup
+
 
 @borg.on(admin_cmd(pattern=("coronatr ?(.*)"))) # pylint:disable=E0602
 async def cor_tr(event):
-    x = await event.edit("`Corona virüs bilgileri sağlık bakanlığından alınıyor.`")
-    await asyncio.sleep(3)
-    if not os.path.isdir('./DOWNLOADS/'):
-        os.makedirs('./DOWNLOADS/')
-    r = requests.get(
-    'https://covid19.saglik.gov.tr/')
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        res1 = soup.find_all("img",class_="img-fluid")
-        resimler = []
-        for image in res1:
-            resimler.append(image['src'])
-        re1 = resimler[2]
-        resim1 = "https://covid19.saglik.gov.tr/{}".format(re1)
-        wget.download(resim1, out='./DOWNLOADS/1.jpg')
-        re2 = resimler[3]
-        resim2 = "https://covid19.saglik.gov.tr/{}".format(re2)
-        wget.download(resim2, out='./DOWNLOADS/2.jpg')
-        img1 = Config.TMP_DOWNLOAD_DIRECTORY + '2.jpg'
-        await event.client.send_file(
-            event.chat_id,
-            img1,
-            force_document=False,
-            allow_cache=False,
-            caption="Sağlık Bakanlığı Corona Virüs Güncel Bilgiler Grafiği",
-            reply_to=event
-        )
-        await  asyncio.sleep(4)
-        img2 = Config.TMP_DOWNLOAD_DIRECTORY + '1.jpg'
-        await event.client.send_file(
-            event.chat_id,
-            img2,
-            force_document=False,
-            allow_cache=False,
-            caption="Sağlık Bakanlığı Corona Virüs Güncel Bilgiler Tablosu",
-            reply_to=event
-        )
-        os.remove(img1)
-        await asyncio.sleep(3)
-        os.remove(img2)
-    else:
-        await x.edit("`Sağlık Bakanlığı sitesine erişilemiyor veya bir sorun var. Hata Kodu : 404`")
+    x = await event.edit("`Corona Virüs Bilgileri https://covid19.saglik.gov.tr/ adresinden alınıyor..`")
+    try:
+        r = requests.get("https://covid19.saglik.gov.tr/")
+        if r.status_code == 200:
+            sayfa_linki = requests.get('https://covid19.saglik.gov.tr/')
+            soup =  BeautifulSoup(sayfa_linki.content, 'html.parser')
+            vakalar = soup.find_all(class_="list-group list-group-genislik")
+            vaka = vakalar[0].text.split()
+            toplam_test = vaka[3]
+            toplam_vaka = vaka[7]
+            toplam_olum = vaka[11]
+            toplam_yog_bakım = vaka[17]
+            entube = vaka[22]
+            iyilesen = vaka[27]
 
+
+            bugun_test = soup.find(class_="buyuk-bilgi-l-sayi").text
+            bugun_= soup.find_all("span",class_="")
+            bugun_vaka = bugun_[13].text
+            bugun_vefat = bugun_[15].text
+            bugun_iyilesen = bugun_[17].text
+
+            msg = """
+            Sağlık Bakanlığı Corona Virus Bilgileri
+            Kaynak: `https://covid19.saglik.gov.tr/`
+
+            *Toplam Test:* {}
+            *Toplam Vaka:* {}
+            *Toplam Vefat:* {}
+            *Toplam Yoğun Bakımdaki Hasta:* {}
+            *Toplam Entübe:* {}
+            *Toplam İyileşen:* {}
+
+            Bugünün Verileri
+
+            *Bugünkü Test:* {}
+            *Bugünkü Vaka:* {}
+            *Bugünkü Vefat:* {}
+            *Bugünkü İyileşen:* {}
+            """.format(
+                toplam_test,
+                toplam_vaka,
+                toplam_olum,
+                toplam_yog_bakım,
+                entube,iyilesen,
+                bugun_test,
+                bugun_vaka,
+                bugun_vefat,
+                bugun_iyilesen
+            )
+        else:
+            await event.edit("`Bakanlık sitesine bağlanırken sorun oluştu`")
+    except HTMLParseError as err:
+        await event.edit("`Hata oluştu. Hata kodu: {}`".format(str(err)))
