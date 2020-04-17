dedc674b6016c8d50e59cc9e7cdc54446a8f20ae
M.Furkan <byazade@yandex.com>
2020-04-17 20:07:47 +0300

    edit

--- stdplugins/inactive_users.py
+++ stdplugins/inactive_users.py
@@ -1,8 +1,6 @@
-import asyncio
-import logging
 from asyncio import sleep
 from os import remove
-
+import asyncio
 from telethon import events
 from telethon.errors import (BadRequestError, ChatAdminRequiredError,
                              ImageProcessFailedError, PhotoCropSizeSmallError,
@@ -21,82 +19,71 @@ from telethon.tl.types import (ChannelParticipantsAdmins,
 from sample_config import Config
 from uniborg.util import admin_cmd
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-logger = logging.getLogger(__name__)
 
 KICK_RIGHTS = ChatBannedRights(
     until_date=None,
     view_messages=True
 )
 
-@borg.on(admin_cmd(pattern="inactive ?(.*)"))  
+@borg.on(events.NewMessage(outgoing=True, pattern="^.inactive ?(.*)"))  
 async def list_users(event):
-    logger.info("çalışıyor")
     if not event.is_group:
         await event.edit("Are you sure this is a group?")
         return
-    b = []
-    f = {}
-    l = []
-    x = ""
-    msg = event.pattern_match.group(1)
-    if not event.pattern_match.group(1):
+
+    try:
+        msg = event.pattern_match.group(1)
+        mesaj = "Grupta {} mesajı olmayanlar otomatik olarak çıkarılıyor..\nBu uzun sürebilir".format(msg)
+        await event.edit(mesaj)
+        b = []
+        f = {}
+        l = []
+        x = ""
+        msg = event.pattern_match.group(1)
+        # if not event.pattern_match.group(1):
         async for user in event.client.iter_participants(event.chat_id):
-            b.append(user.id)
+            # print(user)
+            if not user.bot:
+                # print(user.first_name)
+                b.append(user.id)
             # print(user.id)
         p = 0
         h = 0
         while len(b)>h:
         # for h in range(len(b)):
             if h==p:
-                async for messages in event.client.iter_messages(event.chat_id,from_user=b[h],wait_time=3,reverse=True):
-                    # x = await event.client.get_messages(event.chat_id,messages.id)
-                    # print(x.message)
+                async for messages in event.client.iter_messages(event.chat_id,from_user=b[h],reverse=True):
                     l.append(messages.from_id)
                     l=list(set(l))
                     x +="{}\t{}\n".format(messages.from_id,messages.text)
-                    # l.append(messages.from_id,messages.text)
-                    # print(messages.from_id,messages.text)
             p = p+1
             h = h+1
-            print(x)
-            # print(x)
-            # print()
-            # print(l)
-            print(l)
+            liste = []
             for t in range(len(l)):
-                # print(x.count(str(l[t])))
-                print(x.count(str(l[t])))
-            # print(x.count('\n'))
-            # if messages.from_id in b:
-                # print(messages.message)
-            # b.append(user)
-        # print(l)
+                liste.append(x.count(str(l[t])))
+            for j in range(len(l)):
+                if int(liste[j]) < int(msg):
+                    l.append(l[j])
+                    l=list(set(l))
+                    print(l[j])
+                    await event.client(
+                        EditBannedRequest(
+                            event.chat_id,
+                            l[j],
+                            KICK_RIGHTS
+                        )
+                    )
+    except BadRequestError as bad:
+        await event.edit(str(bad))
+    except ChatAdminRequiredError as ch:
+        await event.edit(str(ch))
         
-        # print([b[j] for j in range(len(b))])
-        # async for messages in event.client.iter_messages(event.chat_id,from_user=await event.client.get_input_entity([b[j] for j in range(len(b))]),wait_time=3):
-        #     print(messages.message)
-        # async for messages in event.client.iter_messages(event.chat_id,from_user=b,wait_time=3):
-            # print(messages)
-            # if not user.deleted:
-                # b.append(user)
-                # mentions += f"\n{user.id}"
-            # else:
-                # b.append(user)
-                # mentions += f"\n{user.id}"
-    # else:
-    #     searchq = event.pattern_match.group(1)
-    #     async for user in event.client.iter_participants(event.chat_id, search=f'{searchq}'):
-            # if not user.deleted:
-            #     b.append(user)
-            #     mentions += f"\n{user.id}"
-            # else:
-            #     b.append(user)
-            #     mentions += f"\n{user.id}"
-        # a=[]
-        # async for messages in event.client.iter_messages(event.chat_id,from_user=await event.client.get_entity(b),wait_time=3):
-        #     print(messages.message)
-        #     a.append(messages.message)
-        # print(len(a))
+
+
+async def ban_user(chat_id, i, rights):
+    try:
+        await borg(functions.channels.EditBannedRequest(chat_id, i, rights))
+        return True, None
+    except Exception as exc:
+        return False, str(exc)
 
