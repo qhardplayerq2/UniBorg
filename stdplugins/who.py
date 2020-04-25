9ad14259996fc9be1a7baff3326bc013e82a0918
hamitdurmus <55110331+hamitdurmus@users.noreply.github.com>
2020-04-26 01:27:18 +0300

    Add files via upload

--- stdplugins/who.py
+++ stdplugins/who.py
@@ -1,15 +1,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+import asyncio
 import html
+import logging
 
-from telethon import events
-from telethon import utils
+from telethon import events, utils
+from telethon.errors.rpcerrorlist import MessageTooLongError
+from telethon.events import NewMessage
 from telethon.tl import types
 
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+def split_message(text, length=4096, offset=200):
+    return [text[text.find('\n', i - offset, i + 1) if text.find('\n', i - offset, i + 1) != -1 else i:
+                 text.find('\n', i + length - offset, i + length) if text.find('\n', i + length - offset,
+                                                                               i + length) != -1 else i + length] for
+            i
+            in
+            range(0, len(text), length)]
 
 def get_who_string(who):
     who_string = html.escape(utils.get_display_name(who))
@@ -39,17 +50,26 @@ async def _(event):
 async def _(event):
     members = []
     async for member in borg.iter_participants(event.chat_id):
-        messages = await borg.get_messages(
-            event.chat_id,
-            from_user=member,
-            limit=0
-        )
-        members.append((
-            messages.total,
-            f"{messages.total} - {get_who_string(member)}"
-        ))
+        if not member.deleted and not member.bot:
+            messages = await borg.get_messages(
+                event.chat_id,
+                from_user=member,
+                limit=0
+            )
+            members.append((
+                messages.total,
+                f"{messages.total} - {get_who_string(member)}\n"
+            ))
     members = (
         m[1] for m in sorted(members, key=lambda m: m[0], reverse=True)
     )
-
-    await event.edit("\n".join(members), parse_mode='html')
+    members = "".join(members)
+    try:
+        await event.reply(members, parse_mode='html')
+    except MessageTooLongError:
+        # print("too message")
+        for m in split_message(members):
+            # print(m)
+            await asyncio.sleep(2)
+            await event.reply(f"{m}", parse_mode="html")
+    del members
