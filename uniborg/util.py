4fb5cdd8e502974e5328998cded3f7f68e16c345
M.Furkan <byazade@yandex.com>
2020-04-25 13:08:04 +0300

    fix

--- uniborg/util.py
+++ uniborg/util.py
@@ -109,21 +109,45 @@ async def is_read(borg, entity, message, is_out=None):
 #             tmp
 #         ))
 
+# async def progress(current,total,event,start,type_of_ps):
+# 	"""Faster progressbar
+# 	"""
+# 	now = time.time()
+# 	diff = now - start
+# 	if round(diff % 10.00) == 0 or current == total:
+#         percentage = current * 100 / total
+#         speed = current / diff
+#         elapsed_time = round(diff) * 1000
+#         time_to_completion = round((total - current) / speed) * 1000
+#         estimated_total_time = elapsed_time + time_to_completion
+#         with alive_bar(100, bar='blocks') as bar:
+#         	for i in range(100):
+#                 await asyncio.sleep(5)
+#                 progress_str = bar()
+#         tmp = progress_str + \
+#             "{0} of {1}\nETA: {2}".format(
+#                 humanbytes(current),
+#                 humanbytes(total),
+#                 time_formatter(estimated_total_time)
+#             )
+#         await event.edit("{}\n {}".format(
+#             type_of_ps,
+#             tmp
+#         ))
+
 async def progress(current,total,event,start,type_of_ps):
-	"""Faster progressbar
-	"""
-	now = time.time()
-	diff = now - start
-	if round(diff % 10.00) == 0 or current == total:
-		percentage = current * 100 / total
+    now = time.time()
+    diff = now -start
+    if round(diff % 10.00)== 0 or current == total:
+        percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
+        time_to_completion = round((total - current)/speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         with alive_bar(100, bar='blocks') as bar:
-        	for i in range(100):
-			await asyncio.sleep(5)
-			progress_str = bar()
+            for i in range(100):
+                await asyncio.sleep(5)
+                progress_str = bar()
         tmp = progress_str + \
             "{0} of {1}\nETA: {2}".format(
                 humanbytes(current),
@@ -134,7 +158,6 @@ async def progress(current,total,event,start,type_of_ps):
             type_of_ps,
             tmp
         ))
-
 
 def humanbytes(size):
     """Input size in bytes,
