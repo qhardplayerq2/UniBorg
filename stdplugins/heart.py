47531c655be40604f0b3191a146d52c2b45efd76
unknown <byazade@yandex.com>
2020-05-11 14:14:56 +0300

    new heart

--- stdplugins/heart.py
+++ stdplugins/heart.py
@@ -7,18 +7,27 @@ from collections import deque
 
 from telethon import events
 
-  
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
-@borg.on(events.NewMessage(pattern=r"\.kalp", outgoing=True))  
+
+@borg.on(events.NewMessage(pattern=r"\.kalp", outgoing=True))
 async def _(event):
-	if event.fwd_from:
-		return
-	deq = deque(list("💙💛💓💔💘💕💜💚💝💞💟"))
-	for _ in range(100):
-		await asyncio.sleep(0.1)
-		await event.edit("".join(deq))
-		deq.rotate(1)
+    if event.fwd_from:
+        return
+    deq = deque(list("💙💛💓💔💘💕💜💚💝💞💟"))
+    for _ in range(100):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@borg.on(events.NewMessage(pattern="bkalp", outgoing=True))
+async def bkallp(event):
+    a = "❤️ 🧡 💛 💚 💙 💜 🖤 🤍 🤎 💔 💘 💝".split(" ")
+    d = await event.reply("...")
+    for t in a:
+        await d.edit(t)
+        await asyncio.sleep(5)
