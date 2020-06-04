a77897c70693d84266d466b4dc7fa16f4bdbf9e3
unknown <byazade@yandex.com>
2020-06-04 22:30:44 +0300

    testing mongodb and snip

--- sample_config.py
+++ sample_config.py
@@ -3,6 +3,8 @@
 # Create a new config.py file in same dir and import, then extend this class.
 import os
 from distutils.util import strtobool as sb
+from pymongo import MongoClient
+from redis import StrictRedis
 
 
 class Config(object):
@@ -22,17 +24,20 @@ class Config(object):
     if PRIVATE_GROUP_BOT_API_ID:
         PRIVATE_GROUP_BOT_API_ID = int(PRIVATE_GROUP_BOT_API_ID)
     # Send .get_id in any channel to fill this value. ReQuired for @Manuel15 inspiration to work!
-    PRIVATE_CHANNEL_BOT_API_ID = os.environ.get("PRIVATE_CHANNEL_BOT_API_ID", None)
+    PRIVATE_CHANNEL_BOT_API_ID = os.environ.get(
+        "PRIVATE_CHANNEL_BOT_API_ID", None)
     if PRIVATE_CHANNEL_BOT_API_ID:
         PRIVATE_CHANNEL_BOT_API_ID = int(PRIVATE_CHANNEL_BOT_API_ID)
     # This is required for the plugins involving the file system.
-    TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
+    TMP_DOWNLOAD_DIRECTORY = os.environ.get(
+        "TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
     # This is required for the speech to text module. Get your USERNAME from https://console.bluemix.net/docs/services/speech-to-text/getting-started.html
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     IBM_WATSON_CRED_USERNAME = os.environ.get("IBM_WATSON_CRED_USERNAME", None)
     # This is required for the hash to torrent file functionality to work.
-    HASH_TO_TORRENT_API = os.environ.get("HASH_TO_TORRENT_API", "https://example.com/torrent/{}")
+    HASH_TO_TORRENT_API = os.environ.get(
+        "HASH_TO_TORRENT_API", "https://example.com/torrent/{}")
     # This is required for the @telegraph functionality.
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "UniBorg")
     # Get a Free API Key from OCR.Space
@@ -53,7 +58,8 @@ class Config(object):
     # TG API limit. A message can have maximum 4096 characters!
     MAX_MESSAGE_SIZE_LIMIT = 4095
     # set blacklist_chats where you do not want userbot's features
-    UB_BLACK_LIST_CHAT = {int(x) for x in os.environ.get("UB_BLACK_LIST_CHAT", "").split()}
+    UB_BLACK_LIST_CHAT = {int(x) for x in os.environ.get(
+        "UB_BLACK_LIST_CHAT", "").split()}
     # specify LOAD and NO_LOAD
     LOAD = []
     # foloowing plugins won't work on Heroku,
@@ -73,7 +79,8 @@ class Config(object):
     # DataBase would not work
     DB_URI = os.environ.get("DATABASE_URL", None)
     # number of rows of buttons to be displayed in .helpme command
-    NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD", 5))
+    NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD = int(
+        os.environ.get("NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD", 5))
     # specify command handler that should be used for the plugins
     # this should be a valid "regex" pattern
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "\.")
@@ -91,7 +98,8 @@ class Config(object):
     #
     TELE_GRAM_2FA_CODE = os.environ.get("TELE_GRAM_2FA_CODE", None)
     #
-    GROUP_REG_SED_EX_BOT_S = os.environ.get("GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot")
+    GROUP_REG_SED_EX_BOT_S = os.environ.get(
+        "GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot")
     # rapidleech plugins
     OPEN_LOAD_LOGIN = os.environ.get("OPEN_LOAD_LOGIN", "0")
     OPEN_LOAD_KEY = os.environ.get("OPEN_LOAD_KEY", "0")
@@ -100,13 +108,13 @@ class Config(object):
     GOOGLE_CHROME_DRIVER = os.environ.get("GOOGLE_CHROME_DRIVER", None)
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
     # spotify stuff
-    DEFAULT_BIO = os.environ.get("DEFAULT_BIO",None)
-    SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX",None)
-    SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS",None)
-    SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME",None)
+    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+    SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
+    SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
+    SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME", None)
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
     LYDIA_API = os.environ.get("LYDIA_API", None)
-    DEFAULT_NAME = os.environ.get("DEFAULT_NAME",None)
+    DEFAULT_NAME = os.environ.get("DEFAULT_NAME", None)
     VIRUSTOTAL_API_KEY = os.environ.get("VIRUSTOTAL_API_KEY", None)
     #
     # define "spam" in PMs
@@ -121,15 +129,15 @@ class Config(object):
     YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
     ANTI_SPAMBOT = os.environ.get("ANTI_SPAMBOT", None)
     ANTI_SPAMBOT_SHOUT = os.environ.get("ANTI_SPAMBOT_SHOUT", None)
-    #API_TOKEN for quote plugin
-    API_TOKEN = os.environ.get("API_TOKEN",None)
+    # API_TOKEN for quote plugin
+    API_TOKEN = os.environ.get("API_TOKEN", None)
     # MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     BOTLOG = os.environ.get("BOTLOG", None)
     # MONGOCLIENT = pymongo.MongoClient(MONGO_DB_URI)
     # MONGO = MONGOCLIENT.userbot
-    #MIRRORACE KEY  
-    MIRROR_ACE_API_KEY = os.environ.get("MIRROR_ACE_API_KEY",None)
-    MIRROR_ACE_API_TOKEN = os.environ.get("MIRROR_ACE_API_TOKEN",None)
+    # MIRRORACE KEY
+    MIRROR_ACE_API_KEY = os.environ.get("MIRROR_ACE_API_KEY", None)
+    MIRROR_ACE_API_TOKEN = os.environ.get("MIRROR_ACE_API_TOKEN", None)
     # RSS_POST_MSG_GROUP_ID = map(int, os.environ.get("RSS_POST_MSG_GROUP_ID", None).split())
     RSS_POST_MSG_GROUP_ID = os.environ.get("RSS_POST_MSG_GROUP_ID", None)
     if RSS_POST_MSG_GROUP_ID:
@@ -137,8 +145,26 @@ class Config(object):
     SPAM_WATCH_API = os.environ.get("SPAM_WATCH_API", None)
     # Userbot logging feature switch.
     LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", None))
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    MONGOCLIENT = MongoClient(MONGO_DB_URI, 27017, serverSelectionTimeoutMS=1)
+    MONGO = MONGOCLIENT.HU_STRING_SESSION
+    REDIS = StrictRedis(host='localhost', port=6379, db=0)
 
 
+def is_redis_alive():
+    try:
+        Config.REDIS.ping()
+        return True
+    except BaseException:
+        return False
+
+
+def is_mongo_alive():
+    try:
+        Config.MONGOCLIENT.server_info()
+    except BaseException:
+        return False
+    return True
 
 
 class Production(Config):
@@ -147,4 +173,3 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
-
