1d06d74455994386ccec43d784a57cf57a49700f
unknown <byazade@yandex.com>
2020-06-07 16:56:48 +0300

    new plugin

---
+++ stdplugins/music.py
@@ -0,0 +1,37 @@
+"get music from .m <music query>  Credits https://t.me/By_Azade"
+
+import logging
+
+from sample_config import Config
+from uniborg.util import admin_cmd
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+@borg.on(admin_cmd(pattern="m ?(.*)"))  # pylint:disable=E0602
+async def music_find(event):
+    if event.fwd_from:
+        return
+
+    music_name = event.pattern_match.group(1)
+    msg = await event.get_reply_message()
+    if music_name:
+        await event.delete()
+        song_result = await event.client.inline_query("deezermusicbot", music_name)
+
+        await song_result[0].click(
+            event.chat_id,
+            reply_to=event.reply_to_msg_id,
+            hide_via=True
+        )
+    elif msg:
+        await event.delete()
+        song_result = await event.client.inline_query("deezermusicbot", msg.message)
+
+        await song_result[0].click(
+            event.chat_id,
+            reply_to=event.reply_to_msg_id,
+            hide_via=True
+        )
