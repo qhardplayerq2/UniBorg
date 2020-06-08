a0ad53431b7e690e036b1ffe958e323985918da8
unknown <byazade@yandex.com>
2020-06-08 12:14:45 +0300

    new inline bot added

--- stdplugins/music.py
+++ stdplugins/music.py
@@ -21,17 +21,41 @@ async def music_find(event):
         await event.delete()
         song_result = await event.client.inline_query("deezermusicbot", music_name)
 
-        await song_result[0].click(
+        await event.respond(await song_result[0].click(
             event.chat_id,
             reply_to=event.reply_to_msg_id,
             hide_via=True
-        )
+        ))
     elif msg:
         await event.delete()
         song_result = await event.client.inline_query("deezermusicbot", msg.message)
 
-        await song_result[0].click(
+        await event.respond(await song_result[0].click(
             event.chat_id,
             reply_to=event.reply_to_msg_id,
             hide_via=True
+        ))
+
+
+@borg.on(admin_cmd(pattern="spotbot ?(.*)"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    # msg = await event.get_reply_message()
+    # print(msg)
+    music_name = event.pattern_match.group(1)
+    msg = await event.get_reply_message()
+    # print(event)
+    if music_name:
+        await event.delete()
+        song_result = await event.client.inline_query("spotify_to_mp3_bot", music_name)
+        await event.respond(
+            await song_result[1].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
+        )
+    elif msg:
+        await event.delete()
+        song_result = await event.client.inline_query("spotify_to_mp3_bot", msg.message)
+
+        await event.respond(
+            await song_result[1].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
         )
