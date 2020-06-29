421cf2348d95b01a8560c5b67e90eca1712dbfd8
unknown <byazade@yandex.com>
2020-06-29 20:10:18 +0300

    new music dl plugin :-)

--- stdplugins/music.py
+++ stdplugins/music.py
@@ -1,5 +1,11 @@
 "get music from .m <music query>  Credits https://t.me/By_Azade"
 import logging
+
+from telethon import events
+from telethon.errors.rpcerrorlist import (UserAlreadyParticipantError,
+                                          YouBlockedUserError)
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+
 from uniborg.util import admin_cmd
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
@@ -102,3 +108,33 @@ async def _(event):
                 await j.delete()
                 await k.edit("Kanal Linki:\nhttps://t.me/joinchat/AAAAAE8NqbV48l7ls-pFtQ")
                 break
+
+
+@borg.on(admin_cmd(pattern="ad ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.edit("```Reply to any user message.```")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await event.edit("```reply to media message```")
+        return
+    chat = "@audiotubebot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+        await event.edit("```Reply to actual users message.```")
+        return
+    await event.edit("```Processing```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(events.NewMessage(
+                incoming=True, from_users=507379365))
+            await event.client.send_message(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Please unblock @AudioTubeBot and try again```")
+            return
+        await event.delete()
+        await event.client.send_file(event.chat_id, response.message.media)
