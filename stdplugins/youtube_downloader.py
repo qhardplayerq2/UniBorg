84333dd061c9914e8994aecd79cab7224a625ce6
unknown <byazade@yandex.com>
2020-07-08 12:27:08 +0300

    edit

--- stdplugins/youtube_downloader.py
+++ stdplugins/youtube_downloader.py
@@ -194,7 +194,7 @@ async def download_video(v_url):
                       if any(fn.endswith(ext) for ext in included_extensions)]
 
         thumb = out_folder + "cover.jpg"
-        file_path = relevant_path + file_names[0]
+        file_path = out_folder + file_names[0]
         song_size = file_size(file_path)
         await v_url.edit(f"`Preparing to upload song:`\
         \n**{ytdl_data['title']}**\
@@ -225,7 +225,7 @@ async def download_video(v_url):
         file_names = [fn for fn in os.listdir(relevant_path)
                       if any(fn.endswith(ext) for ext in included_extensions)]
 
-        file_path = relevant_path + file_names[0]
+        file_path = out_folder + file_names[0]
         video_size = file_size(file_path)
         thumb = out_folder + "cover.jpg"
 
