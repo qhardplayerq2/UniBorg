c94fa84ab59affa523cbf92f35664126479a12c5
unknown <byazade@yandex.com>
2020-07-13 21:59:21 +0300

    fix

--- stdplugins/youtube_downloader.py
+++ stdplugins/youtube_downloader.py
@@ -191,7 +191,7 @@ async def download_video(v_url):
         # song_size = size(raster_size)
         thumb = out_folder + f"{ytdl_data['id']}.webp"
         im = Image.open(thumb).convert("RGB")
-        im.save("thumb.jpeg", "jpeg")
+        im.save(out_folder + "thumb.jpeg", "jpeg")
         thumb = out_folder + "thumb.jpeg"
         file_path = f"{out_folder + ytdl_data['id']}.mp3"
         song_size = file_size(file_path)
@@ -228,7 +228,7 @@ async def download_video(v_url):
             image = f"{ytdl_data['id']}.jpg"
             thumb = out_folder + f"{ytdl_data['id']}.webp"
             im = Image.open(thumb).convert("RGB")
-            im.save("thumb.jpeg", "jpeg")
+            im.save(out_folder + "thumb.jpeg", "jpeg")
             thumb = out_folder + "thumb.jpeg"
             await v_url.edit(f"`Preparing to upload video:`\
             \n**{ytdl_data['title']}**\
