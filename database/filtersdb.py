9b5bd3715dc7c9dac2baab19bd1db0e2f29e7be3
unknown <byazade@yandex.com>
2020-07-18 11:56:14 +0300

    new db plugin for mongodb

--- database/filtersdb.py
+++ database/filtersdb.py
@@ -3,7 +3,7 @@ from database.mongo import cli
 cli = cli["Userbot"]["Filters"]
 
 
-async def get_filters(chatid):
+async def get_all_filters(chatid):
     return cli.find({'chat_id': chatid})
 
 
@@ -11,14 +11,14 @@ async def get_filter(chatid, keyword):
     return cli.find_one({'chat_id': chatid, 'keyword': keyword})
 
 
-async def add_filter(chatid, keyword, msg):
+async def add_filter(chatid, keyword, msg_id):
     to_check = await get_filter(chatid, keyword)
 
     if not to_check:
         cli.insert_one({
             'chat_id': chatid,
             'keyword': keyword,
-            'msg': msg
+            'msg': msg_id
         })
         return True
     else:
@@ -28,7 +28,7 @@ async def add_filter(chatid, keyword, msg):
                 'chat_id': to_check["chat_id"],
                 'keyword': to_check["keyword"],
             }, {"$set": {
-                'msg': msg
+                'msg': msg_id
             }})
 
         return False
@@ -43,8 +43,11 @@ async def delete_filter(chatid, keyword):
         cli.delete_one({
             '_id': to_check["_id"],
             'chat_id': to_check["chat_id"],
-            'keyword': to_check["keyword"],
-            'msg': to_check["msg"]
+            'keyword': to_check["keyword"]
         })
 
         return True
+
+
+async def delete_all_filters(chatid):
+    return cli.delete_many({"chat_id": chatid})
