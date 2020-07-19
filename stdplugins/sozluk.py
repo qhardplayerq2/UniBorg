f50dda01276cca517ec478fd66e74a78af13e646
unknown <byazade@yandex.com>
2020-07-19 14:09:58 +0300

    new plugin for me :)

--- stdplugins/sozluk.py
+++ stdplugins/sozluk.py
@@ -19,17 +19,20 @@ async def sozluk(event):
     if not word:
         await event.edit("anlamını öğrenmek istediğiniz kelimeyi girin")
     else:
-        r_req = requests.get(
-            f"https://api.dictionaryapi.dev/api/v1/entries/tr/{word}")
-        r_dec = r_req.json()
-        r_dec = r_dec[0]
-        meaning = r_dec['meaning']['ad']
-        anlamlar = "**{} kelimesinin anlamları:**".format(word.upper())
-        ornekler = "**{} kelimesinin örnekleri:**".format(word.upper())
-        for j in meaning:
-            if "definition" in j:
-                anlamlar += "\n" + "👉  " + j['definition']
-                if "example" in j:
-                    ornekler += "\n" + "👉  " + j['example']
-        out = anlamlar + "\n\n" + ornekler
-        await event.edit(out)
+        try:
+            r_req = requests.get(
+                f"https://api.dictionaryapi.dev/api/v1/entries/tr/{word}")
+            r_dec = r_req.json()
+            r_dec = r_dec[0]
+            meaning = r_dec['meaning']['ad']
+            anlamlar = "**{} kelimesinin anlamları:**".format(word.upper())
+            ornekler = "**{} kelimesinin örnekleri:**".format(word.upper())
+            for j in meaning:
+                if "definition" in j:
+                    anlamlar += "\n" + "👉  " + j['definition']
+                    if "example" in j:
+                        ornekler += "\n" + "👉  " + j['example']
+            out = anlamlar + "\n\n" + ornekler
+            await event.edit(out)
+        except:
+            await event.edit("hata oluştu")
